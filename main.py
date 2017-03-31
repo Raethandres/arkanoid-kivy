bd13ba21edd2c09eaed6496b14bd564ad1fa5c3c
Raethandres <raethandres4@gmail.com>
2017-03-31 07:24:37 -0700

    cambios

--- main.py
+++ main.py
@@ -9,57 +9,103 @@ from kivy.properties import NumericProperty, ReferenceListProperty,\
 from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.uix.carousel import Carousel
+#from kivy.label import Label
+from kivy.uix.image import Image
 #from plyer import accelerometer
 
 
 class ArkanivyBrick(Widget):
-    def bounce_ball(self, ball):
-        if self.collide_widget(ball):
+    
+
+    def var(self):
+        self.dic={'1':self.level1,'2':self.level2}
+        self.im=[]
+        self.p=0
+
+    def level1(self):
+        y=1
+        if self.im:
+            self.im=[]
+        else:
+            self.im.append(Image(source='/resorce/'+str(y)+'.png' ,x=500 ,y=300,size=(56, 26)))
+            self.im.append(Image(source='/resorce/'+str(y)+'.png' ,x=556 ,y=300,size=(56, 26)))
+            # self.im.append(Image(source=str(y)+'.png' ,x=612 ,y=300,size=(56, 26)))
+            # self.im.append(Image(source=str(y)+'.png' ,x=444 ,y=300,size=(56, 26)))
+            # self.im.append(Image(source=str(y)+'.png' ,x=388 ,y=300,size=(56, 26)))
+            # self.im.append(Image(source=str(y)+'.png' ,x=332 ,y=300,size=(56, 26)))
+            # self.im.append(Image(source=str(y)+'.png' ,x=276 ,y=300,size=(56, 26)))
+            # self.im.append(Image(source=str(y)+'.png' ,x=220 ,y=300,size=(56, 26)))
+            self.im.append(Image(source='/resorce/'+str(y)+'.png' ,x=164 ,y=300,size=(56, 26)))
+        return self.im
+
+    def level2(self):
+        y=1
+        if self.im:
+            self.im=[]
+        else:
+            self.im.append(Image(source='/resorce/'+str(y)+'.png' ,x=500 ,y=300))
+            self.im.append(Image(source='/resorce/'+str(y)+'.png' ,x=556 ,y=300))
+            self.im.append(Image(source='/resorce/'+str(y)+'.png' ,x=612 ,y=300))
+            self.im.append(Image(source='/resorce/'+str(y)+'.png' ,x=444 ,y=300))
+            self.im.append(Image(source='/resorce/'+str(y)+'.png' ,x=388 ,y=300))
+            self.im.append(Image(source='/resorce/'+str(y)+'.png' ,x=332 ,y=300))
+            self.im.append(Image(source='/resorce/'+str(y)+'.png' ,x=276 ,y=300))
+            self.im.append(Image(source='/resorce/'+str(y)+'.png' ,x=220 ,y=300))
+            self.im.append(Image(source='/resorce/'+str(y)+'.png' ,x=164 ,y=300))
+        return self.im
+
+    def bounce_ball(self,ball,index):
+        
+
+        if self.im[index].collide_widget(ball):
+            #print 'df'
             vx, vy = ball.velocity
+            offset = 0
             bounced = Vector(vx, -1 * vy)
-            print "ArkanivyBrick** ",bounced
-            vel = bounced * 1.02
+            vel = bounced * 1.1
             if ball.center_x>self.center_x:
                 if vel.x<-0.5:
-                    ball.velocity = vel.x+1, vel.y
+                    ball.velocity = vel.x+1, vel.y+offset
                 else:
-                    ball.velocity=vel.x+0.5, vel.y
+                    ball.velocity=vel.x+0.5, vel.y+offset
             elif ball.center_x<self.center_x:
                 if vel.x>0.5:
-                    ball.velocity = vel.x-1, vel.y
+                    ball.velocity = vel.x-1, vel.y+offset
                 else:
-                    ball.velocity = vel.x-0.5, vel.y
+                    ball.velocity = vel.x-0.5, vel.y+offset
             else:
-                ball.velocity = vel.x, vel.y
+                ball.velocity = vel.x, vel.y+offset
+
             return True
         return False
     
-    def on_parent(self, widget, parent):
-        if parent is not None:
-            parent.bricks.append(self)
+    # def on_parent(self, widget, parent):
+    #     if parent is not None:
+    #         parent.bricks.append(self)
 
 
 class ArkanivyPaddle(Widget):
     score = NumericProperty(0)
     sw=0
+    g=0.5
     def bounce_ball(self, ball):
         if self.collide_widget(ball):
             vx, vy = ball.velocity
+            offset = 0
             bounced = Vector(vx, -1 * vy)
-            print "ArkanivyPaddle** ",bounced
-            vel = bounced * 1.02
+            vel = bounced * 1.1
             if ball.center_x>self.center_x:
                 if vel.x<-0.5:
-                    ball.velocity = vel.x+1, vel.y
+                    ball.velocity = vel.x+1, vel.y+offset
                 else:
-                    ball.velocity=vel.x+0.5, vel.y
+                    ball.velocity=vel.x+0.5, vel.y+offset
             elif ball.center_x<self.center_x:
                 if vel.x>0.5:
-                    ball.velocity = vel.x-1, vel.y
+                    ball.velocity = vel.x-1, vel.y+offset
                 else:
-                    ball.velocity = vel.x-0.5, vel.y
+                    ball.velocity = vel.x-0.5, vel.y+offset
             else:
-                ball.velocity = vel.x, vel.y
+                ball.velocity = vel.x, vel.y+offset
 
 class ArkanivyBall(Widget):
     velocity_x = NumericProperty(0)
@@ -68,34 +114,54 @@ class ArkanivyBall(Widget):
 
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
+        print 'move'
+
+
 
 
 class ArkanivyGame(Widget):
     ball = ObjectProperty(None)
     player = ObjectProperty(None)
-    bricks =  ListProperty([])
+    #bricks =  ListProperty([])
+    bricks= ArkanivyBrick()
     sw=0
     sb=0
 
-    def serve_ball(self, vel=(0, 4)):
+    def load_level(self,l):
+        self.bricks.var()
+        f=self.bricks.dic[l]
+        f()
+        for x in self.bricks.im:
+            #print 'aja'
+            self.add_widget(x)
+
+
+    def serve_ball(self, vel=(0, 1)):
+        print 'serve'
         self.ball.center_x = self.player.center_x
         self.ball.center_y = self.player.center_y+30
         self.ball.velocity = vel
 
     def update(self, dt):
         self.ball.move()
+
         # bounce of paddles
         self.player.bounce_ball(self.ball)
 
-        for brick in self.bricks:
-            if brick.bounce_ball(self.ball):
-                try:
-                    self.remove_widget(brick)
-                    brick.center_x=brick.center_x+10000
-                    self.player.score+=1
-                except:
-                    brick.center_x=brick.center_x+10000
-                    self.player.score+=1
+        for x in range(len(self.bricks.im)-1):
+            #print x
+            if self.bricks.bounce_ball(self.ball,x):
+                self.remove_widget(self.bricks.im[x])
+                self.bricks.im[x].center_x=self.bricks.im[x].center_x+10000
+                self.bricks.im.pop(x)
+                self.player.score+=1
+        #         try:
+        #             self.remove_widget(brick)
+        #             brick.center_x=brick.center_x+10000
+        #             self.player.score+=1
+        #         except:
+        #             brick.center_x=brick.center_x+10000
+        #             self.player.score+=1
             #self.remove_widget(self.bricks)
             #self.bricks.remove(brick)
 
@@ -109,12 +175,13 @@ class ArkanivyGame(Widget):
         if self.ball.y < self.y:
             self.returnBall()
 
-        if self.player.score>=13:
+        if self.player.score>=2:
+            #self.load_level('2')
             # self.bricks[0].center_x=100
             # self.bricks[0].center_y=100
             # self.returnBall()
             # self.add_widget(self.bricks[0])
-            returnBall()
+            self.returnBall()
             return False
 
     def returnBall(self):
@@ -124,7 +191,7 @@ class ArkanivyGame(Widget):
         self.ball.velocity = (0,0)
 
     def on_touch_down(self, touch):
-        if touch.x>self.player.center_x-(self.player.width) and  touch.x<self.player.center_x+(self.player.width) and  touch.y<self.player.center_y+(self.player.height/2) :
+        if touch.x>self.player.center_x-(self.player.width) and  touch.x<self.player.center_x+(self.player.width)  and  touch.y<self.player.center_y+(self.player.height/2) :
             self.sw=1
         if touch.x>self.ball.center_x-(self.ball.width) and  touch.x<self.ball.center_x+(self.ball.width) and touch.y>self.ball.center_y-(self.ball.height/2) and  touch.y<self.ball.center_y+(self.ball.height/2) :
             self.serve_ball()
@@ -138,16 +205,22 @@ class ArkanivyGame(Widget):
         self.sw=0
 
 
+
 class ArkanivyApp(App):
-    def build(self):
+    def level1(self):
         game = ArkanivyGame()
-        #caro=Carousel()
-        #caro.add_widget(game)
-        #caro.add_widget(gam)
-       #game.serve_ball()
-        #Builder.apply_file(game, 'level1.kv')
+        game.load_level('1')
         Clock.schedule_interval(game.update, 1.0 / 60.0)
-        print 'ok'
+        return game
+    def level2(self):
+        game = ArkanivyGame()
+        game.load_level('2')
+        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        return game
+    def build(self):
+        game=self.level1()
+        #game=self.level2()
+        
         return game
 
 
